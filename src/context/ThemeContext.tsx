10d84cfeae613f9b03bfe51eb60b214e06dab470
Aryan Bhandari <aryanbhandari4077@gmail.com>
2025-09-06 21:59:14 +0530

    Deploy

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,31 +1,46 @@
 // ThemeContext.tsx
-import  { createContext, useMemo, useState, useContext, type ReactNode } from "react";
-import { ThemeProvider, CssBaseline } from "@mui/material";
-import { createAppTheme } from "../components/layout/Theme";
+import {
+  createContext,
+  useMemo,
+  useState,
+  useContext,
+  type ReactNode,
+  useEffect
+} from 'react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import { createAppTheme } from '../components/layout/Theme'
 
-type ThemeMode = "light" | "dark";
+type ThemeMode = 'light' | 'dark'
 
 interface ThemeContextProps {
-  mode: ThemeMode;
-  toggleMode: () => void;
+  mode: ThemeMode
+  toggleMode: () => void
 }
 
-const ThemeModeContext = createContext<ThemeContextProps | undefined>(undefined);
+const ThemeModeContext = createContext<ThemeContextProps | undefined>(undefined)
 
 export const useThemeMode = () => {
-  const ctx = useContext(ThemeModeContext);
-  if (!ctx) throw new Error("useThemeMode must be used within ThemeModeProvider");
-  return ctx;
-};
+  const ctx = useContext(ThemeModeContext)
+  if (!ctx)
+    throw new Error('useThemeMode must be used within ThemeModeProvider')
+  return ctx
+}
 
 export const ThemeModeProvider = ({ children }: { children: ReactNode }) => {
-  const [mode, setMode] = useState<ThemeMode>("light");
+  const [mode, setMode] = useState<ThemeMode>('light')
+
+  useEffect(() => {
+    const stored = localStorage.getItem('theme') as ThemeMode | null
+    if (stored) {
+      setMode(stored)
+    }
+  }, []);
 
   const toggleMode = () => {
-    setMode((prev) => (prev === "light" ? "dark" : "light"));
-  };
+    setMode(prev => (prev === 'light' ? 'dark' : 'light'))
+  }
 
-  const theme = useMemo(() => createAppTheme(mode), [mode]);
+  const theme = useMemo(() => createAppTheme(mode), [mode])
 
   return (
     <ThemeModeContext.Provider value={{ mode, toggleMode }}>
@@ -34,5 +49,5 @@ export const ThemeModeProvider = ({ children }: { children: ReactNode }) => {
         {children}
       </ThemeProvider>
     </ThemeModeContext.Provider>
-  );
-};
+  )
+}
